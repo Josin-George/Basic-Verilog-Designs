c8e57a8e0864fb0873af87a01e1aaf70f7d73b4a
Michael_j_nj <georgejosin6@gmail.com>
2025-10-26 17:09:29 +0530

    Range Counters

--- RangeUpCounter/counter_10_to_40_up.v
+++ RangeUpCounter/counter_10_to_40_up.v
@@ -7,7 +7,9 @@ module counter_10_to_40_up(count,clk,rst,data);
     reg [7:0] count_temp;
     
     always@(posedge clk)
-        if(!rst|count_temp>=8'd40| count_temp<8'd10)
+        if(!rst | count_temp>=8'd40 | count_temp<8'd10)
+            count_temp<=8'd10;
+        else
             count_temp <= count_temp + 1;
 assign count = count_temp;
 
